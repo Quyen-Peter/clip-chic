f0642c6a64a860c882b10226a953b996405f595b
Quyen-Peter <quyentrinh040@gmail.com>
2025-09-16 15:18:12 +0700

    design order history page

--- src/screens/account/OrderHistory.tsx
+++ src/screens/account/OrderHistory.tsx
@@ -1,13 +1,67 @@
 import "../../css/OrderHistory.css";
 
-
-
-const OrderHistory = () =>{
-     return(
-        <div className="history-container">
-           <a>history-container</a>
+const OrderHistory = () => {
+  return (
+    <div className="history-container">
+      <div className="table-order">
+        <h3 className="order-history-content">ORDER HISTORY</h3>
+        <div className="order-history">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>ORDER ID</th>
+                <th>STATUS</th>
+                <th>DATE</th>
+                <th>TOTAL</th>
+                <th>ACTION</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <a href="#">#96459761</a>
+                </td>
+                <td className="status in-progress">IN PROGRESS</td>
+                <td>Dec 30, 2019 07:52</td>
+                <td>405.000 vnd (5 Products)</td>
+                <td>
+                  <a href="#" className="view-details">
+                    View Details →
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a href="#">#71667167</a>
+                </td>
+                <td className="status completed">COMPLETED</td>
+                <td>Dec 7, 2019 23:26</td>
+                <td>350.000 vnd (4 Products)</td>
+                <td>
+                  <a href="#" className="view-details">
+                    View Details →
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a href="#">#95214362</a>
+                </td>
+                <td className="status canceled">CANCELED</td>
+                <td>Dec 7, 2019 23:26</td>
+                <td>165.000 vnd (2 Products)</td>
+                <td>
+                  <a href="#" className="view-details">
+                    View Details →
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default OrderHistory;
