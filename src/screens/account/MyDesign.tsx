254992768203a83767dad32997d98f0f9077a5b0
Quyen-Peter <quyentrinh040@gmail.com>
2025-09-23 14:10:30 +0700

    design page

--- src/screens/account/MyDesign.tsx
+++ src/screens/account/MyDesign.tsx
@@ -1,11 +1,58 @@
 import "../../css/MyDesign.css";
+import search from "../../assest/search.png";
+import React, { useEffect, useState } from "react";
 
-const MyDesign = () =>{
-     return(
-        <div className="my-design-container">
-            <a>my design</a>
-        </div>
-    )
+interface Product {
+  id: number;
+  name: string;
+  image: string;
+  current: boolean;
 }
+
+const MyDesign = () => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [design, setDesign] = useState<Product[]>([]);
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    fetch("/design.json")
+      .then((res) => res.json())
+      .then((data) => setDesign(data));
+  }, []);
+
+  return (
+    <div className="my-design-container">
+      <h3 className="history-title">HISTORY WORK</h3>
+      <div>
+        <form className="my-design-form">
+          <input
+            type="text"
+            placeholder="Search in browsing history"
+            className="my-design-input"
+          />
+          <button type="submit" className="my-design-button">
+            <img className="icon-search-bnt" src={search} />
+          </button>
+        </form>
+      </div>
+
+      <div className="my-design-content">
+        <h5 className="history-work-title">HISTORY WORK</h5>
+        <div className="my-design-list">
+          {design.map((item) => (
+            <div className="my-design-item" key={item.id}>
+              <img className="my-design-image" src={item.image} />
+              <p className="my-design-name">{item.name}</p>
+              <button className="my-design-button-use">Edit</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MyDesign;
